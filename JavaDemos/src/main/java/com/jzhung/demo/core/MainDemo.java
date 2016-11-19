7e4a1f9b58d610e5351277be280cb6825f909bd0
jzhung <i@jzhung.com>
2016-11-19 15:35:02 +0800

    First commit

--- JavaDemos/src/main/java/com/jzhung/demo/core/MainDemo.java
+++ JavaDemos/src/main/java/com/jzhung/demo/core/MainDemo.java
@@ -4,4 +4,7 @@ package com.jzhung.demo.core;
  * Created by jzhung on 2016/11/19.
  */
 public class MainDemo {
+    public static void main(String[] args) {
+        System.out.println("nice");
+    }
 }
