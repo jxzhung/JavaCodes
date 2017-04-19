bd34c829437ad2f3387c0bd9823b7197c95a29c7
jzhung <i@jzhung.com>
2017-04-19 09:50:36 +0800

    专题sql生成

---
+++ JavaDemos/src/main/java/com/jzhung/demo/arithmetic/sorting/SotringMain.java
@@ -0,0 +1,22 @@
+package com.jzhung.demo.arithmetic.sorting;
+
+import org.junit.Test;
+
+/** 常见排序算法
+ * Created by Jzhung on 2017/3/29.
+ */
+public class SotringMain {
+    private int[] num = {1, 2, 56, 43, 52, 12, 73, 34, 68, 57, 92, 96};
+
+    /**
+     * 冒泡排序
+     */
+    @Test
+    public void bubble(){
+        int max;
+        for (int i = 0; i < num.length; i++) {
+
+
+        }
+    }
+}
